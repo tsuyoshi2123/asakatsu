4d098a635aace7e6eb6b688dea1f5a05ae45e728
髙橋剛志 <tt2123244@gmail.com>
2022-09-08 22:25:11 +0900

    fontawesome 設定

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,7 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/sass/header.scss', 'public/css')
+.sass('resources/sass/reset.scss', 'public/css')
+.sass('resources/assets/sass/app.scss', 'public/fontawesome/css');
