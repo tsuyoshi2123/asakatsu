02af8e2d865eb3ef12e8b9b21eecde4762dff4a2
髙橋剛志 <tt2123244@gmail.com>
2022-09-10 14:31:28 +0900

    header 修正

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -11,16 +11,17 @@
     <title>asakatsu</title>
 </head>
 <body>
-    <div class="header">
-        <p class="header-title">asakatsu</p>
-        <div class="header-box">
+    <div class="heade">
+        <p class="heade__title">asakatsu</p>
+        <div class="heade__box">
             <table style="height: 30px">
                 <tr><td>継続/累計</td></tr>
                 <tr><td>10/20</td></tr>
             </table>
-            <img src="/image/fuji.png" alt="/" class="header-box-pic">
-            <i class="fa-solid fa-caret-down header-box-triangle"></i>
+            <img src="/image/fuji.png" alt="/" class="heade__box--pic">
+            <i class="fa-solid fa-caret-down heade__box--triangle"></i>
         </div>
     </div>
+    @yield('content')
 </body>
 </html>
