ff622071de51ef75cc21e2f4a37345ac3e53888f
髙橋剛志 <tt2123244@gmail.com>
2022-09-10 14:39:43 +0900

    add : content

--- src/resources/views/report.blade.php
+++ src/resources/views/report.blade.php
@@ -1,1 +1,33 @@
 @extends('header')
+@section('content')
+    <div class="wrapper">
+        <div class="outer">
+            <div class="outer__heading">
+                <p class="outer__heading__title">マップ</p>
+                <p class="outer__heading__lead">テスト区テスト市テスト町<br>テストマンション1-1-1<br>テストマンション1-1-1</p>
+            </div>
+            <div class="outer__index">
+                <p class="outer__index__tag weekly">週間</p>
+                <p class="outer__index__tag months">月間</p>
+                <p class="outer__index__tag years">年間</p>
+            </div>
+            <div class="outer__list">
+                <p class="outer__list__title"></p>
+                <p class="outer__list__section"></p>
+                <p class="outer__list__section"></p>
+                <p class="outer__list__section"></p>
+            </div>
+        </div>
+        <div class="area">
+            <p class="area__time">2022 09/05 12:24:15</p>
+            <div class="area__list">
+                <div class="area__list__user">
+                    <p class="name">カフェ太郎</p>
+                    <p class="comment">カフェレビュー</p>
+                    <p class="continued">継続：10</p>
+                    <p class="cumulative">累計：20</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
