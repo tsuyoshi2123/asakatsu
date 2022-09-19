0a9ebe5aeb5dbec2e0e7fa53b8d2302e3a9f0140
髙橋剛志 <tt2123244@gmail.com>
2022-09-19 17:34:58 +0900

    link 指定 scss で纏めたものを指定

--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/css/content.css">
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="{{ mix('fontawesome/css/app.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
     <title>asakatsu</title>
 </head>
 <body>
