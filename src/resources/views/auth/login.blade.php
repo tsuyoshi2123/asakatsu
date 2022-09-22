f93fd80ba0c0e6d3a5a8ced663ce3fbb047b6b72
髙橋剛志 <tt2123244@gmail.com>
2022-09-22 21:51:32 +0900

    add : error-message @csrf fix : button

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -3,33 +3,48 @@
 @section('content')
 <div class="login-container">
     <form method="POST" action="{{ route('login') }}">
+        @csrf
         <div class="card__section">
             <p class="card__section__title">{{ __('Login') }}</p>
         </div>
         <div class="card__contents">
             <div class="card__contents__box">
                 <div class="card__contents__box__item">
-                    <p class="form-box">
-                        <label class="email" for="email">{{ __('Email') }}</label>
-                        <input type="text" name="email" id="email" class="form-control">
-                    </p>
+                    <div>
+                        <p class="form-box">
+                            <label class="email" for="email">{{ __('Email') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        </p>
+                        @error('email')
+                        <span class="help-block" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
                 <div class="card__contents__box__item">
-                    <p class="form-box" for="password">
-                        <label class="password">{{ __('Password') }}</label>
-                        <input type="text" name="password" id="password" class="form-control">
-                    </p>
+                    <div>
+                        <p class="form-box" for="password">
+                            <label class="password">{{ __('Password') }}</label>
+                            <input type="text" name="password" id="password" class="form-control" value="{{ old('password') }}">
+                        </p>
+                        @error('password')
+                        <span class="help-block" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
             </div>
             <div class="form__check">
                 <p class="form__check__wrap">
-                    <input name="remember" type="checkbox" class="form__check__wrap--input">
+                    <input name="remember" type="checkbox" class="form__check__wrap--input" {{ old('remember') ? 'checked' : '' }}>
                     <label for="remember" id="remember" class="form__check__wrap--remember">
                         {{ __('Remember Me') }}
                     </label>
-                    <a href="{{ route('login') }}" class="btn--login">
+                    <button type="submit" class="btn--login">
                         {{ __('Login') }}
-                    </a>
+                    </button>
                 </p>
             </div>
             <div class="forgot__box">
