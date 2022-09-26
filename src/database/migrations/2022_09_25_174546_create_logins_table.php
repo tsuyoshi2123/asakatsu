3949179b40952c2578fb631e018e86adc5517f69
髙橋剛志 <tt2123244@gmail.com>
2022-09-26 21:52:25 +0900

    add : logins テーブル作成

---
+++ src/database/migrations/2022_09_25_174546_create_logins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logins', function (Blueprint $table) {
+            $table->integer('user_id')->unique();
+            $table->date('login_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logins');
+    }
+}
