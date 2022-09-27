43c99a3da2f5a63e3296dbf15b9f3abff7d09be5
髙橋剛志 <tt2123244@gmail.com>
2022-09-27 21:08:02 +0900

    add : 総計カラム追加

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('comment')->nullable();
             $table->string('file_name')->default('fuji.png');
+            $table->integer('total_cumulative')->default(1);
             $table->integer('total_continuation')->default(1);
             $table->rememberToken();
             $table->timestamps();
