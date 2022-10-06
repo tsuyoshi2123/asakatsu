8da084a0d0ddc1d79ecad60525cd0f666d61c905
髙橋剛志 <tt2123244@gmail.com>
2022-10-07 05:43:06 +0900

    create : DateCount mfsc

---
+++ src/database/migrations/2022_10_07_054233_create_date_counts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDateCountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('date_counts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('date_counts');
+    }
+}
