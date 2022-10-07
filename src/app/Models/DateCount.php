586c5187270fc3f9620fef84d8622121fea177bb
髙橋剛志 <tt2123244@gmail.com>
2022-10-07 23:26:05 +0900

    リレーション設定

--- src/app/Models/DateCount.php
+++ src/app/Models/DateCount.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class DateCount extends Model
 {
     use HasFactory;
+
+    /**
+     * users テーブルのリレーションの設定
+     * @return HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
--- src/app/Models/DateStart.php
+++ src/app/Models/DateStart.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class DateStart extends Model
 {
     use HasFactory;
+
+    /**
+     * date_counts テーブルのリレーションの設定
+     * @return HasOne
+     */
+    public function DateCount(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
