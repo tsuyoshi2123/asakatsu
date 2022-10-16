07a871b393ccc727b75b4757f359fc6d79d55b3b
髙橋剛志 <tt2123244@gmail.com>
2022-10-16 20:56:08 +0900

    User リレーション設定

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,4 +44,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * usersテーブルのリレーションの設定
+     * @return HasOne
+     */
+    public function DateCount(): HasOne
+    {
+        return $this->hasOne(DateCount::class, 'user_id');
+    }
 }
